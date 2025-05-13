b551e7374f853907ed65d0a1c983f2fb8c761242
Alexander G. Morano <amorano@gmail.com>
2025-05-13 03:12:40 -0400

    initial pass

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
-# Jovi_Preset
-Presets for ComfyUI Nodes
+<div align="center">
+<picture>
+  <source srcset="https://raw.githubusercontent.com/Amorano/Jovimetrix-examples/refs/heads/master/res/logo-jovi_preset.png">
+  <img alt="Presets for ComfyUI Nodes with user persistance" width="384" height="384">
+</picture>
+</div>
+<div align="center">
+<a href="https://github.com/comfyanonymous/ComfyUI">COMFYUI</a> Presets for ComfyUI Nodes with user persistance<br><br>
+</div>
+
+<div align="center">
+
+![KNIVES!](https://badgen.net/github/open-issues/Amorano/Jovi_Preset)
+![FORKS!](https://badgen.net/github/forks/Amorano/Jovi_Preset)
+
+</div>
+
+<!---------------------------------------------------------------------------->
+
+# SPONSORSHIP
+
+Please consider sponsoring me if you enjoy the results of my work, code or documentation or otherwise. A good way to keep code development open and free is through sponsorship.
+
+<div align="center">
+
+&nbsp;|&nbsp;|&nbsp;|&nbsp;
+-|-|-|-
+[![BE A GITHUB SPONSOR ❤️](https://img.shields.io/badge/sponsor-30363D?style=for-the-badge&logo=GitHub-Sponsors&logoColor=#EA4AAA)](https://github.com/sponsors/Amorano) | [![DIRECTLY SUPPORT ME VIA PAYPAL](https://img.shields.io/badge/PayPal-00457C?style=for-the-badge&logo=paypal&logoColor=white)](https://www.paypal.com/paypalme/onarom) | [![PATREON SUPPORTER](https://img.shields.io/badge/Patreon-F96854?style=for-the-badge&logo=patreon&logoColor=white)](https://www.patreon.com/joviex) | [![SUPPORT ME ON KO-FI!](https://ko-fi.com/img/githubbutton_sm.svg)](https://ko-fi.com/alexandermorano)
+</div>
+
+## HIGHLIGHTS
+
+<div align="center">
+<img src="https://github.com/user-attachments/assets/f23d96d7-edaf-44d9-924b-6c6636b625c3" alt="Jovi Presets" width="768"/>
+<img src="https://github.com/user-attachments/assets/61509883-7567-4990-b9ef-1e83084840fb" alt="Jovi Presets" width="768"/>
+</div>
+
+## UPDATES
+
+**2025/05/13** @1.0.0:
+* intial commit
+
+# INSTALLATION
+
+## COMFYUI MANAGER
+
+If you have [ComfyUI Manager](https://github.com/ltdrdata/ComfyUI-Manager) installed, simply search for Jovi_Help and install from the manager's database.
+
+## MANUAL INSTALL
+Clone the repository into your ComfyUI custom_nodes directory. You can clone the repository with the command:
+```
+git clone https://github.com/Amorano/Jovi_Preset.git
+```
+You can then install the requirements by using the command:
+```
+.\python_embed\python.exe -s -m pip install -r requirements.txt
+```
+If you are using a <code>virtual environment</code> (<code><i>venv</i></code>), make sure it is activated before installation. Then install the requirements with the command:
+```
+pip install -r requirements.txt
+```
+# WHERE TO FIND ME
+
+You can find me on [![DISCORD](https://dcbadge.vercel.app/api/server/62TJaZ3Z5r?style=flat-square)](https://discord.gg/62TJaZ3Z5r).
--- web/presets.js
+++ web/presets.js
@@ -2,23 +2,23 @@
 
 import { app } from "../../scripts/app.js";
 
-class UIPresetManager {
+const SETTINGS = "jovi.preset";
+const PRESET = app.extensionManager.setting.get(SETTINGS) || {};
+
+class PresetManager {
     constructor(node) {
         this.node = node;
-        // Store presets configuration
-        this.presets = {
-            default : {}
-        };
-
-        // everyone has a default...
-        if (node.widgets) {
-            for (const widget of Object.values(node.widgets)) {
-                this.presets.default[widget.name] = widget.value;
-            }
-        }
+        this.presets = PRESET[this.node.title] || {};
     }
 
     apply(name) {
+        if (name === "default") {
+            for (const widget of Object.values(this.node.widgets)) {
+                widget.value = widget.options?.default;
+            }
+            return;
+        }
+
         const preset = this.presets?.[name];
         if (preset) {
             for (const [key, value] of Object.entries(preset)) {
@@ -31,10 +31,43 @@ class UIPresetManager {
             app.graph.setDirtyCanvas(!0, !1);
         }
     }
+
+    async del(name) {
+        if (name === "default") {
+            throw new Error("Cannot delete 'default' preset");
+        }
+        delete this.presets[name];
+        PRESET[this.node.title] = this.presets;
+        if (Object.keys(PRESET[this.node.title] || {}).length === 0) {
+            delete PRESET[this.node.title];
+        }
+        await app.extensionManager.setting.set(SETTINGS, PRESET);
+    }
+
+    async save(name) {
+        name = name.toLowerCase();
+        if (name === "default") {
+            throw new Error("Cannot overwrite 'default' preset");
+        }
+
+        this.presets[name] = {}
+        for (const widget of Object.values(this.node.widgets)) {
+            this.presets[name][widget.name] = widget.value;
+        }
+
+        PRESET[this.node.title] = this.presets;
+        await app.extensionManager.setting.set(SETTINGS, PRESET);
+    }
 }
 
 app.registerExtension({
-    name: "jov.uiPresetManager",
+    name: "jovi.preset",
+    settings: [{
+        id: SETTINGS,
+        name: "Presets for nodes",
+        type: "hidden",
+        defaultValue: {}
+    }],
     async beforeRegisterNodeDef(nodeType, nodeData) {
         const onNodeCreated = nodeType.prototype.onNodeCreated;
         nodeType.prototype.onNodeCreated = function() {
@@ -42,30 +75,57 @@ app.registerExtension({
             if (!this.widgets) {
                 return me;
             }
-            this.presetManager = new UIPresetManager(this, nodeData);
+            this.presetManager = new PresetManager(this, nodeData);
             return me;
         };
 
         const getExtraMenuOptions = nodeType.prototype.getExtraMenuOptions;
-        nodeType.prototype.getExtraMenuOptions = function (_, options) {
+        nodeType.prototype.getExtraMenuOptions = function (canvas, options) {
             const me = getExtraMenuOptions?.apply(this, arguments);
-            if (this.widgets) {
-                const presetList = [];
-                for (const presetName of Object.keys(this.presetManager.presets)) {
-                    const presetObject = {
-                        content: `${presetName}`,
-                        callback: () => this.presetManager.apply(presetName)
-                    };
-                    presetList.push(presetObject);
-                }
-                options.push({
-                    content: "Presets",
-                    submenu: {
-                        options: presetList
-                    }
-                })
+            if (this.widgets === undefined || this.widgets.length == 0) {
+                return me;
             }
+
+            const presetList = [{
+                content: "Save Preset...",
+                callback: () => {
+                    const prompt = canvas.prompt("Save Preset", "", async function(v) {
+                        if (/^[A-Za-z\s]+$/.test(v)) {
+                            try {
+                                await this.presetManager.save(v);
+                            } catch(e) {
+                                throw new Error(e);
+                            }
+                        }
+                    }.bind(this));
+                    if (prompt && prompt.style) {
+                        prompt.style.position = "fixed";
+                        prompt.style.left = `${canvas.last_mouse[0]}px`;
+                        prompt.style.top = `${canvas.last_mouse[1]}px`;
+                    }
+                }
+            }];
+
+            for (const presetName of Object.keys(this.presetManager.presets)) {
+                const presetObject = {
+                    content: presetName,
+                    callback: async (a, _, click) => {
+                        if (click.ctrlKey) {
+                            await this.presetManager.del(presetName);
+                        } else {
+                            this.presetManager.apply(presetName);
+                        }
+                    }
+                };
+                presetList.push(presetObject);
+            }
+            options.push({
+                content: "Presets",
+                submenu: {
+                    options: presetList
+                }
+            })
             return me;
-        };
+        }
     }
 });
